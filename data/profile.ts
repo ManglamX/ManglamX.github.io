f3177488230b3f047e1bec5843b6bdfabcef4122
ManglamX <ecs.crce.10127@gmail.com>
2025-12-08 21:36:43 +0530

    resume link update

--- data/profile.ts
+++ data/profile.ts
@@ -2,5 +2,5 @@ export const profile = {
   name: "MANGLAM JAISWAL",
   github: "https://github.com/ManglamX",
   linkedin: "https://www.linkedin.com/in/manglam-jaiswal-0b2822290",
-  resumeUrl: "https://drive.google.com/file/d/1yWVis5NKXKcqo1oSuxovFo2tFAKDYeEL/view?usp=sharing",
+  resumeUrl: "https://drive.google.com/file/d/14Th3jxaasNEvsqOHrbysqMGGfpSUYibD/view?usp=sharing",
 }
